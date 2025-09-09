6fda5cc1fb7b520cc4b665ea0e3380c77980bdae
HAELGEE <helge1337@live.com>
2025-09-09 12:34:29 +0200

    Small start

---
+++ Entity/Member.cs
@@ -0,0 +1,12 @@
+﻿namespace Entity;
+
+public class Member
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int? Age { get; set; }
+    public string? UserName { get; set; }
+    public bool IsAdmin { get; set; } = false;
+
+}
