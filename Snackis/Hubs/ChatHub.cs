45b7703726153d3f9a6463df265882f203ec04e7
HAELGEE <helge1337@live.com>
2025-09-09 13:07:42 +0200

    Better start

---
+++ Snackis/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace YourProjectName.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Message(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            //this will be listen by client using javascript.
+        }
+    }
+}
